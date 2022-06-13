7e0b81c17fc23b87cfdda105285a50f83a455e23
babydev-by03KOREA <hyeongju1122@gmail.com>
2022-06-13 19:35:32 +0900

    책 상세조회 페이지 제작완료!

--- SampleSpringYse/src/main/java/sample/spring/yse/BookService.java
+++ SampleSpringYse/src/main/java/sample/spring/yse/BookService.java
@@ -3,8 +3,9 @@ package sample.spring.yse;
 import java.util.Map;
 
 public interface BookService {
-
+	// 메소드 시그니처 생성
+	
 	String create(Map<String, Object> map);
 
-	
+	Map <String, Object> detail(Map<String, Object> map);
 }
